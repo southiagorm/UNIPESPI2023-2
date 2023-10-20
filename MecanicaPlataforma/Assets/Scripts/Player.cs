d42905264bf5b3c6adb60905fd18bb43a0683c75
Thiago R <southiagorm@gmail.com>
2023-10-20 10:24:09 -0300

    aula dia 19-10

--- MecanicaPlataforma/Assets/Scripts/Player.cs
+++ MecanicaPlataforma/Assets/Scripts/Player.cs
@@ -6,12 +6,29 @@ public class Player : MonoBehaviour
 {
     private Animator _anim;
     private SpriteRenderer _sprite;
+    private Rigidbody2D _rb;
+
     private float _h;
+
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _jumpForce;
+
+    [SerializeField]
+    private GameObject _ground;
+    [SerializeField]
+    private LayerMask _layerMask;
+
+    [SerializeField]
+    private bool _isGrounded;
 
     private void Start()
     {
         _anim = GetComponent<Animator>();
         _sprite = GetComponent<SpriteRenderer>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -24,6 +41,24 @@ public class Player : MonoBehaviour
         {
             _sprite.flipX = false;
         }
+        Jump();
         _anim.SetFloat("Run", Mathf.Abs(_h));
+        _anim.SetFloat("Jump", _rb.velocity.y);
+        _anim.SetBool("IsGround", _isGrounded);
+    }
+
+    private void FixedUpdate()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_ground.transform.position, 0.3f, _layerMask);
+        _rb.velocity = new Vector2(_h*_speed*Time.deltaTime, _rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            print("Pular");
+            _rb.AddForce(new Vector2(0,_jumpForce), ForceMode2D.Impulse);
+        }
     }
 }
